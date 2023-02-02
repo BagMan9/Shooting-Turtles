9c53b96975cf3e32231d510cdfc714d47543211b
Isaac Grannis <115715725+BagMan9@users.noreply.github.com>
2023-02-02 16:18:12 -0500

    Bullets in semi working state

--- soldiers.py
+++ soldiers.py
@@ -3,7 +3,7 @@ import turtle
 import asyncio
 
 
-def wiggle(red, blue):
+async def movement(red, blue):
     r_turtle = random.choice(red)
     if 20 < r_turtle.heading() < 340:
         r_turtle.seth(random.randrange(0, 25))
@@ -12,29 +12,35 @@ def wiggle(red, blue):
     b_turtle.seth(random.randrange(155, 205))
 
 
-async def bullet(launcher):
-    bul = launcher.clone()
-    bul.color("black")
-    bul.seth(launcher.heading())
-    travel = 0
-    while travel < 700:
-        bul.forward(5)
-        travel += 5
-    bul.hideturtle()
+async def bullet(shooter):
+    print("Confirmed")
+    if random.randrange(0, 50) <= 5:
+        bul = shooter.clone()
+        bul.color("black")
+        bul.seth(shooter.heading())
+        travel = 0
+        while travel < 700:
+            bul.forward(5)
+            bul.pos()
+            travel += 5
+        bul.hideturtle()
+        bul.clear()
+        del bul
+    else:
+        pass
 
 
 # Simulation loop
 
-async def sim_loop(red, blue):
-    wiggle(red, blue)
-    if random.randrange(0, 50) <= 5:
-        asyncio.run(bullet(random.choice(red)))
+async def main(red, blue):
+    await asyncio.gather(bullet(random.choice(red)), bullet(random.choice(blue)), movement(red, blue))
 
 
 # Initial Spawn
-
-red_list = [turtle.Turtle(), turtle.Turtle(), turtle.Turtle(), turtle.Turtle()]
-blue_list = [turtle.Turtle(), turtle.Turtle(), turtle.Turtle(), turtle.Turtle()]
+red_list = [turtle.Turtle(), turtle.Turtle(), turtle.Turtle(), turtle.Turtle(),
+            turtle.Turtle(), turtle.Turtle(), turtle.Turtle()]
+blue_list = [turtle.Turtle(), turtle.Turtle(), turtle.Turtle(), turtle.Turtle(),
+             turtle.Turtle(), turtle.Turtle(), turtle.Turtle()]
 r_count = 0
 b_count = 0
 # Red spawn
@@ -42,19 +48,21 @@ for r in red_list:  # noinspection PyRedeclaration
     r_count += 1
     r.color("red")
     r.penup()
-    r.goto(random.randrange(-300, -158), random.randrange(-110, 110))
+    r.goto(random.randrange(-350, -125), random.randrange(-200, 200))
 # Blue spawn
 for b in blue_list:  # noinspection PyRedeclaration
     b_count += 1
     b.color("blue")
     b.penup()
-    b.goto(random.randrange(158, 300), random.randrange(-110, 110))
+    b.goto(random.randrange(125, 350), random.randrange(-200, 200))
     b.seth(180)
+
+# Main loop
+while r_count > 0 and b_count > 0:
+    asyncio.run(main(red_list, blue_list))
 
 loop = asyncio.get_event_loop()
 asyncio.set_event_loop(loop)
-while r_count > 0 and b_count > 0:
-    asyncio.run(sim_loop(red_list, blue_list))
 screen = turtle.Screen()
 loop.close()
 turtle.done()
